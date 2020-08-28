85bc11bdf329cca6f9cc15d4c712f87d9a97d64d
LeonardoQui <leoqp123r@gmail.com>
2020-08-27 22:33:49 -0500

    clase cliente main funcionando

--- src/Cliente.java
+++ src/Cliente.java
@@ -4,7 +4,36 @@ public class Cliente {
 
     public static void main(String[] args) {
 
-       
+        double totalCuenta;
+        Cuenta Cuenta1;
+        Cuenta1 = new Cuenta(1004012, 50000.70);
+        String nombre;
+        String apellido;
+        String correo;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese los siguientes datos nombre, apellido, correo\n");
+        nombre = sc.nextLine();
+        System.out.print("Nombre: " + nombre+"\n");
+        apellido = sc.nextLine();
+        System.out.print("Apellido: " + apellido+"\n");
+        correo = sc.nextLine();
+
+        totalCuenta = Cuenta1.saldo();
+        System.out.println("Total de saldo actual en la cuenta: " + totalCuenta + " COP");
+        double valconsigna=4000;
+        System.out.println("Se ingresaron en la cuenta: " + valconsigna + "COP");
+        Cuenta1.Consignar(valconsigna);
+
+        double valorretiro=5000;
+        System.out.println( "Se retiraron de la cuenta: "+ valorretiro+ "COP");
+        Cuenta1.Retirar(valorretiro);
+        System.out.println("-----------------------------------------------------------------");
+
+        // Consultamos el saldo de nuevo
+        totalCuenta = Cuenta1.saldo();
+        System.out.println("Datos de usuario: " + "[" + nombre + "," + apellido + "," + correo + "]");
+        System.out.println("Total actual en la cuenta: " + totalCuenta + " COP");
     }
 
 }
