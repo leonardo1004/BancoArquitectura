2a76fa03eb6cd00d2f2e2be4d0b95bd93570fd11
LeonardoQui <leoqp123r@gmail.com>
2020-08-27 22:31:48 -0500

    clase cuenta metodos consignar, retirar y saldo funcionando

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -8,6 +8,20 @@ public class Cuenta {
         saldo = saldoini;
     }
 
+    public double Consignar(double cantidad) {
+        saldo = saldo + cantidad;
+        return cantidad;
+    }
 
+    public void Retirar(double cantidad) {
+        if (saldo<cantidad){
+            System.out.println("No se puede realizar el retiro");
+        } else {
+            saldo = saldo - cantidad;
+        }
+    }
 
+    public double saldo() {
+        return saldo;
+    }
 }
