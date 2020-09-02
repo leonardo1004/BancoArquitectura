cb19e213c4ca1fe2b448174d8200cd8cefc70abc
LeonardoQui <leoqp123r@gmail.com>
2020-09-01 21:32:24 -0500

    output agregado

--- src/TarjetaDebito.java
+++ src/TarjetaDebito.java
@@ -1,7 +1,11 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class TarjetaDebito {
     double saldoTarjetaD;
+    String fechaVencimiento;
 
     public TarjetaDebito(double saldoTarjetaD) {
         this.saldoTarjetaD = saldoTarjetaD;
     }
+    System.out.println("Saldo debito\n")
 }
